25bf7c6d2a338d3eaa958f040fc319fd7cd81ec1
Matt Biddulph <mb@hackdiary.com>
2011-09-28 12:33:54 +0200

    Add language detection bolt

--- src/main/scala/com/hackdiary/storm/WordCountTopology.scala
+++ src/main/scala/com/hackdiary/storm/WordCountTopology.scala
@@ -6,20 +6,23 @@ import backtype.storm.topology._
 import backtype.storm.spout._
 import backtype.storm.tuple._
 import collection.JavaConversions._
-import backtype.storm.utils.Utils.tuple
 import backtype.storm.utils.Utils
 import twitter4j._
 import twitter4j.conf._
 import redis.clients.jedis.Jedis
+import com.cybozu.labs.langdetect._
 
 object WordCountTopology {
   def main(args:Array[String]) = {
-    println(System.getProperties.getProperty("java.class.path"))
+    DetectorFactory.loadProfile("profiles")
+
     val builder = new TopologyBuilder
 
     builder.setSpout(1, new WordSpout)
-    builder.setBolt(2, new WordCount, 8).fieldsGrouping(1, new Fields("words"))
-    builder.setBolt(3, new RedisOut).shuffleGrouping(2)
+    builder.setBolt(2, new LanguageDetect, 8).shuffleGrouping(1)
+    builder.setBolt(3, new SplitTerms, 2).shuffleGrouping(2)
+    builder.setBolt(4, new WordCount, 8).fieldsGrouping(3, new Fields("words"))
+    builder.setBolt(5, new RedisOut).shuffleGrouping(4)
 
     val conf = Map(Config.TOPOLOGY_DEBUG -> false)
 
@@ -55,13 +58,7 @@ class WordSpout extends IRichSpout {
     this.collector = collector
     this.listener = new StatusAdapter { 
       override def onStatus(status : Status) = {
-        val tokenizer = new org.apache.lucene.analysis.standard.StandardTokenizer(org.apache.lucene.util.Version.LUCENE_34,new
-          java.io.StringReader(status.getText))
-        val shingles = new org.apache.lucene.analysis.shingle.ShingleFilter(tokenizer)
-        while(shingles.incrementToken) {
-          val token = shingles.getAttribute(classOf[org.apache.lucene.analysis.tokenattributes.CharTermAttribute])
-          if(token.toString.contains(" ")) collector.emit(tuple(token.toString.toLowerCase))
-        }
+        collector.emit(Utils.tuple(status.getText))
       }
     }
   }
@@ -70,34 +67,71 @@ class WordSpout extends IRichSpout {
   }
 }
 
-class WordCount extends IBasicBolt {
-  var counts : Map[String,Int] = Map.empty
+class SplitTerms extends IRichBolt {
+  var collector : OutputCollector = _
 
-  override def prepare(conf : java.util.Map[_,_], context : TopologyContext) = {
+  override def prepare(conf : java.util.Map[_,_], context : TopologyContext, collector : OutputCollector) = {
+    this.collector = collector
   }
 
-  override def execute(tuple : Tuple, collector : BasicOutputCollector) = {
-    val word : String = tuple.getString(0)
-    var count : Int = counts.getOrElse(word, 0) + 1
-    counts += (word -> count)
-    if(count > 10) collector.emit(new Values(word, count : java.lang.Integer))
+  override def execute(tuple : Tuple) = {
+    val tokenizer = new org.apache.lucene.analysis.standard.StandardTokenizer(org.apache.lucene.util.Version.LUCENE_34,new
+      java.io.StringReader(tuple.getString(0)))
+    val shingles = new org.apache.lucene.analysis.shingle.ShingleFilter(tokenizer,3)
+    while(shingles.incrementToken) {
+      val token = shingles.getAttribute(classOf[org.apache.lucene.analysis.tokenattributes.CharTermAttribute])
+      if(token.toString.contains(" ")) collector.emit(Utils.tuple(token.toString.toLowerCase, tuple.getString(1)))
+    }
+    collector.ack(tuple)
   }
 
   override def cleanup() = {
   }
 
   override def declareOutputFields(declarer : OutputFieldsDeclarer) {
-    declarer.declare(new Fields("word", "count"))
+    declarer.declare(new Fields("words","lang"))
   }
 }
 
-class RedisOut extends IBasicBolt {
-  var jedis : Jedis = _
-  override def prepare(conf : java.util.Map[_,_], context : TopologyContext) = {
-    jedis = new Jedis("localhost")
+class LanguageDetect extends IRichBolt {
+  var collector : OutputCollector = _
+
+  override def prepare(conf : java.util.Map[_,_], context : TopologyContext, collector : OutputCollector) = {
+    this.collector = collector
   }
 
-  override def execute(tuple : Tuple, collector : BasicOutputCollector) = {
+  override def execute(tuple : Tuple) = {
+    val detector = DetectorFactory.create
+    try {
+      detector.append(tuple.getString(0))
+      collector.emit(Utils.tuple(tuple.getString(0),detector.detect))
+      collector.ack(tuple)
+    } catch {
+      case e : Exception => {
+        collector.emit(Utils.tuple(tuple.getString(0),""))
+        collector.ack(tuple)
+      }
+    }
+  }
+
+  override def cleanup() = {
+  }
+
+  override def declareOutputFields(declarer : OutputFieldsDeclarer) {
+    declarer.declare(new Fields("words", "lang"))
+  }
+}
+
+class RedisOut extends IRichBolt {
+  var jedis : Jedis = _
+  var collector : OutputCollector = _
+
+  override def prepare(conf : java.util.Map[_,_], context : TopologyContext, collector : OutputCollector) = {
+    this.collector = collector
+    this.jedis = new Jedis("localhost")
+  }
+
+  override def execute(tuple : Tuple) = {
     //val data = for((field, i) <- tuple.getFields.zipWithIndex) yield (field -> tuple.getValue(i))
     //println(data toMap)
     jedis.set(tuple.getString(0), tuple.getInteger(1).toString)
@@ -110,3 +144,28 @@ class RedisOut extends IBasicBolt {
   override def declareOutputFields(declarer : OutputFieldsDeclarer) {
   }
 }
+
+class WordCount extends IRichBolt {
+  var counts : Map[String,Int] = Map.empty
+  var collector : OutputCollector = _
+
+  override def prepare(conf : java.util.Map[_,_], context : TopologyContext, collector : OutputCollector) = {
+    this.collector = collector
+  }
+
+  override def execute(tuple : Tuple) = {
+    val word : String = tuple.getString(1) + ":" + tuple.getString(0)
+    var count : Int = counts.getOrElse(word, 0) + 1
+    counts += (word -> count)
+    if(count > 10) collector.emit(new Values(word, count : java.lang.Integer))
+    collector.ack(tuple)
+  }
+
+  override def cleanup() = {
+  }
+
+  override def declareOutputFields(declarer : OutputFieldsDeclarer) {
+    declarer.declare(new Fields("word", "count"))
+  }
+}
+
